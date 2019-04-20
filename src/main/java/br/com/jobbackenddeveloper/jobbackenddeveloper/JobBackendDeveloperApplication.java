b0458655124c22c24fbeb72654f8e530c54cdb11
alexmmenendez <alexmmenendez@gmail.com>
2019-04-20 14:56:34 -0300

    first commit

---
+++ src/main/java/br/com/jobbackenddeveloper/jobbackenddeveloper/JobBackendDeveloperApplication.java
@@ -0,0 +1,13 @@
+package br.com.jobbackenddeveloper.jobbackenddeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBackendDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBackendDeveloperApplication.class, args);
+	}
+
+}
