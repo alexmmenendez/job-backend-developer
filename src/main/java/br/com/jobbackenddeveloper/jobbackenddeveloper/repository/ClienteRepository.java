91c3b6ce993abbd28b9cef22b2f9246ab77cdcaa
alexmmenendez <alexmmenendez@gmail.com>
2019-04-21 21:39:25 -0300

    removed unused methods

--- src/main/java/br/com/jobbackenddeveloper/jobbackenddeveloper/repository/ClienteRepository.java
+++ src/main/java/br/com/jobbackenddeveloper/jobbackenddeveloper/repository/ClienteRepository.java
@@ -1,7 +1,6 @@
 package br.com.jobbackenddeveloper.jobbackenddeveloper.repository;
 
 import br.com.jobbackenddeveloper.jobbackenddeveloper.domain.Cliente;
-import br.com.jobbackenddeveloper.jobbackenddeveloper.hardcoded.PerfilEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +11,4 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 	
     Optional<Cliente> findByUsuario(String usuario);
 
-    Optional<Cliente> findByPerfil(PerfilEnum perfil);
-
-    boolean existsByNome(String nome);
 }
